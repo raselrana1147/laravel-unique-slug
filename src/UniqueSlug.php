f3171db6b5cc701aeef87c1436cb1d57d0771aba
raselrana1147 <raselrana1147@gmail.com>
2023-12-30 21:37:31 +0600

    slug generation updated

--- src/UniqueSlug.php
+++ src/UniqueSlug.php
@@ -15,54 +15,24 @@ class UniqueSlug
      * @return string
      * @throws \Exception
      */
-    public function testTime()
+
+    public function generateByModel($model, $value, $field,$separator = null): string
     {
-        return "rasel";
-    }
-    public function generate($model, $value, $field, $separator = null): string
-    {
+
 
         $separator = empty($separator) ? config('laravel-unique-slug.separator') : $separator;
-        $id = 0;
+        $id=0;
+        $slug=null;
 
-        $slug =  preg_replace('/\s+/', $separator, (trim(strtolower($value))));
-        $slug =  preg_replace('/\?+/', $separator, (trim(strtolower($slug))));
-        $slug =  preg_replace('/\#+/', $separator, (trim(strtolower($slug))));
-        $slug =  preg_replace('/\/+/', $separator, (trim(strtolower($slug))));
-
-
-        $slug = preg_replace('![' . preg_quote($separator) . '\s]+!u', $separator, $slug);
-
-
-        $allSlugs = $this->getRelatedSlugs($slug, $id, $model, $field);
-
-
-        if (!$allSlugs->contains("$field", $slug)) {
-            return $slug;
+        $checker=$model::where("$field",'like', $value . '%')->first();
+        if (empty($checker)) {
+            $slug=$value.$separator.$id;
+        }else{
+            $slug=$value.$separator.$checker->id;
         }
-
-
-        for ($i = 1; $i <= config('laravel-unique-slug.max_count'); $i++) {
-            $newSlug = $slug . $separator . $i;
-            if (!$allSlugs->contains("$field", $newSlug)) {
-                return $newSlug;
-            }
-        }
-
-        throw new \Exception('Can not create a unique slug');
-
+        $slug=strtolower($slug);
+        return $slug;
     }
 
-    private function getRelatedSlugs($slug, $id, $model, $field)
-    {
-        if (empty($id)) {
-            $id = 0;
-        }
-
-        return $model::select("$field")
-            ->where("$field", 'like', $slug . '%')
-            ->where('id', '<>', $id)
-            ->get();
-    }
 
 }
